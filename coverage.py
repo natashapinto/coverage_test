1256cbd3b8a77808aaa700872bef5f6980dced30
Natasha Pinto <natasha.b.pinto@gmail.com>
2019-03-10 12:47:26 +0000

    added header

--- coverage.py
+++ coverage.py
@@ -1,1 +1,11 @@
-test
+#!/usr/bin/env/ python
+
+"""
+This script uses the output from Sambamba to generate 
+a report of a list of genes with coverage <100% at 30x 
+
+Author: Natasha Pinto
+Python version: 2.7 
+Date created: 10/03/2019
+"""
+
